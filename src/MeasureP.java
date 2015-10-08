d28393981b87a7613d83461ec942d222da6a3bef
TetsuyaYamanaka <tf14015@student.miyazaki-u.ac.jp>
2015-10-08 18:49:01 +0900

    コミット2（NaNの場合もあるがΔθの値を時系列で導出可能に。改良の余地有）

--- src/MeasureP.java
+++ src/MeasureP.java
@@ -22,17 +22,17 @@ public class MeasureP {
 	static EV3UltrasonicSensor uss = new EV3UltrasonicSensor(SensorPort.S1);
 	static int cnt = 0;
 	static float[] dist = new float[1];
-	static double len,len_p; //障害物との距離
+	//static double len,len_p; //障害物との距離
 	static int rs_A,rs_B; //車輪の回転角速度
 	static float real_v; //車体速度
-	static float a = 0,a_p = 0,d_a; //ロボットの移動距離
+	static float a = 0,a_p = 0; //ロボットの移動距離
 	static float ref_v = 5; //目標車体速度(cm/sec)
 	static double d_theta,theta,theta_p;
 	static double sin_theta,sin_theta_p;
 	static double cos_d_theta;
-	static double[] kyoril_p = new double[DATAMAX];
-	static double[] kyoril = new double[DATAMAX];
-	static double[] kyoria = new double[DATAMAX];
+	static double[] len_p = new double[DATAMAX]; //前時刻の障害物との距離
+	static double[] len = new double[DATAMAX]; //現時刻の障害物との距離
+	static double[] d_a = new double[DATAMAX]; //1サンプリングタイムの走行距離
 	static zahyou[] p_p = new zahyou[DATAMAX], p2_p = new zahyou[DATAMAX]; //前時刻の障害物座標(候補2つ)
 	static zahyou[] p = new zahyou[DATAMAX], p2 = new zahyou[DATAMAX]; //現時刻の障害物座標(候補2つ)
 	
@@ -42,26 +42,19 @@ public class MeasureP {
 		
 		TimerTask Task = new TimerTask(){
 			public void run(){
+				if(cnt != 0){
+					len_p[cnt] = len[cnt-1];
+				}
 				Motor.A.forward(); Motor.B.forward();
 				rs_A = Motor.A.getRotationSpeed(); rs_B = Motor.B.getRotationSpeed();
 				uss.getDistanceMode().fetchSample(dist,0); //動作後の障害物との距離
 				
-				len = dist[0]*100; //m→cm
+				len[cnt] = dist[0]*100; //m→cm
 				real_v = (float)((hankei*(rs_B + rs_A)/2)*(Math.PI/180));
 				a += real_v*(period*0.001);
-				d_a = a - a_p; //1サンプリングタイムの移動距離
-				cos_d_theta = (len*len + len_p*len_p - d_a*d_a)/(2*len*len_p);
-				d_theta = Math.acos(cos_d_theta); //逆三角関数によりΔθを求める
-				sin_theta = len_p*Math.sin(d_theta)/d_a; sin_theta_p = len*Math.sin(d_theta)/d_a;
-				theta = Math.asin(sin_theta); theta_p = Math.asin(sin_theta_p); //逆三角関数によりθ、θ´を求める
+				d_a[cnt] = a - a_p; //1サンプリングタイムの移動距離
 				
-				p_p[cnt].x = len_p*Math.sin(theta_p); p_p[cnt].y = len_p*Math.cos(theta_p);
-				p[cnt].x = len*Math.sin(theta); p[cnt].y = len*Math.cos(theta);
-				p2_p[cnt].x = p_p[cnt].x*-1; p2_p[cnt].y = p_p[cnt].y*-1;
-				p2[cnt].x = p[cnt].x*-1; p2[cnt].y = p[cnt].y*-1;
-				kyoril_p[cnt] = len_p; kyoril[cnt] = len; kyoria[cnt] = d_a;
-				
-				a_p = a; len_p = len;
+				a_p = a;
 				cnt++;
 			}
 		};
@@ -71,7 +64,7 @@ public class MeasureP {
 		Motor.A.setAcceleration(100); Motor.B.setAcceleration(100);
 		Motor.A.setSpeed(ref_v_rol); Motor.B.setSpeed(ref_v_rol);
 		uss.getDistanceMode().fetchSample(dist,0); //動作前の障害物との距離
-		len_p = dist[0]*100; //m→cm
+		len_p[cnt] = dist[0]*100; //m→cm
 		
 		timer.scheduleAtFixedRate(Task, delay, period);
 		Delay.msDelay(4000);
@@ -83,10 +76,22 @@ public class MeasureP {
 			FileWriter fw = new FileWriter("dataP_1008.dat");
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw);
-			pw.println("time\tl'\tl\ta\tP'(x,y)\tP(x,y)");
+			//pw.println("time\tl'\tl\ta\tP'(x,y)\tP(x,y)");
+			pw.println("time\tl'\tl\ta\td_theta");
 			for(i=0;i<cnt;i++){
-				pw.println(i*period + "\t" + kyoril_p[i] + "\t" + kyoril[i] + "\t" + kyoria[i] + "\t" + p_p[cnt].x + ", " + p_p[cnt].y + "\t" + p[cnt].x + ", " + p[cnt].y);
-			}
+				cos_d_theta = (len[i]*len[i] + len_p[i]*len_p[i] - d_a[i]*d_a[i])/(2*len[i]*len_p[i]);
+				d_theta = Math.acos(cos_d_theta); //逆三角関数によりΔθを求める
+				sin_theta = len_p[i]*Math.sin(d_theta)/d_a[i]; sin_theta_p = len[i]*Math.sin(d_theta)/d_a[i];
+				theta = Math.asin(sin_theta); theta_p = Math.asin(sin_theta_p); //逆三角関数によりθ、θ´を求める
+				
+				pw.println(i*period + "\t" + len_p[i] + "\t" + len[i] + "\t" + d_a[i] + "\t" + d_theta);
+				
+				/*p_p[i].x = len_p[i]*Math.sin(theta_p); p_p[i].y = len_p[i]*Math.cos(theta_p);
+				p[i].x = len[i]*Math.sin(theta); p[i].y = len[i]*Math.cos(theta);
+				p2_p[i].x = p_p[i].x*-1; p2_p[i].y = p_p[i].y*-1;
+				p2[i].x = p[i].x*-1; p2[i].y = p[i].y*-1;
+				pw.println(i*period + "\t" + len_p[i] + "\t" + len[i] + "\t" + d_a[i] + "\t" + p_p[i].x + ", " + p_p[i].y + "\t" + p[i].x + ", " + p[i].y);*/
+							}
 			pw.close();
 		}catch(IOException e){
 			System.out.println(e);
